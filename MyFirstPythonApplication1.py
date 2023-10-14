f97406f0a30abd0a3f8b49a3e60b30b064dc1d40
Eric Amoh Adjei <amohericsite@gmail.com>
2023-10-14 18:03:05 -0400

    Add project files.

---
+++ MyFirstPythonApplication1.py
@@ -0,0 +1,41 @@
+'''
+Name : Eric Amoh Adjei
+Date: 10/14/2023
+Assignment 1.1: Your first Python Application
+
+'''
+
+
+# Creating a description as an introduction to Pythoncousre for my users.
+print( "        \t\t\t   -Welcome To Python For Beginners 1O1- \n\n  \t\t\t    A course design with Beginners at Heart. \n\n")
+
+
+# \n represents a newline
+print(" In this Python tutorial, \n\n we will discuss about the history of Python \n\n And Why you should learn Python too ")
+
+
+# Adding the print Python command
+print("But before we begin this exciting journey \n\n You will need to save your progress \n\n And therefore we ask all new students to register basic infomation first")
+
+
+
+# print() is a function that prints a line of text.
+print("Register Here... \n\n ")
+
+
+# Adding at least two variables holding different data types for my application
+name = input("What is your name?\n :")
+age = int( input("What is your age? \n :"))
+print(name + " you are " + str(age) + " years old \n \n ")
+
+print("Thank You Very much " + name + "  for your attention \n\n ")
+
+#adding links to my programming
+print(" Now run Python Online (if needed) https://www.onlinegdb.com/online_python_compiler \n\n ")
+
+#user input
+Fun = input(name + " are you happy to learn Python? \n \n: ")
+print( Fun + " because Python is Python or reptile \n\n ")
+
+#Telling user the lesson is over
+print( name + " thank You Once again for your time \n \n we will end todays lessons here and continue from tomorrow God willing \n \n  THANK YOU ")
