e5bd28f9ac2ffd033505a5ffb86b3af353b08fc0
Amoh Eric <105559568+amoheric@users.noreply.github.com>
2024-09-25 12:08:06 -0400

    Loops and Decision-Making in Python.py

--- Loops and Decision-Making in Python.py
+++ Loops and Decision-Making in Python.py
@@ -3,7 +3,7 @@
 #ASSIGNMENT: Loops and Decision-Making in Python
 
 
-#creating a conditinal python programming with while,for, break, if, elif, and else loops.
+#creating a conditional Python programming with while, for, break, if, elif, and else loops.
 while True:
     
     #\n represents a newline
@@ -22,8 +22,8 @@ while True:
     print("To begin this exciting  journey \n\n  You will need to save your progress \n \n ")
     
     
-    #getting user ready to interact with the application
-    print("\n And therefore we ask all new players to register basic infomation first\n ")
+    #getting the user ready to interact with the application
+    print("\n And therefore we ask all new players to register basic information first\n ")
 
 
     # print() is a function that prints a line of text.
@@ -34,21 +34,21 @@ while True:
     phone = int( input("What is your best phone number? (Int(). only) \n :"))
 
     #this prints user input data with my set string
-    print(name + " your phnone number is " + str(phone) + " right?  \n \n ")
+    print(name + " your phone number is " + str(phone) + " right?  \n \n ")
     
     
     #user input
     Fun = input(name + " are you excited to Play the game? (yes/no) \n \n: ")
-    print( Fun + " because Python is stitll Python, \n nothing changed still a reptile in this game \n\n ")
+    print( Fun + " because Python is still Python, \n nothing changed still a reptile in this game \n\n ")
     
     
     #getting user ready for main sim game
     print(" Are you ready " + name + "  for your first try then? \n\n ")
     
 
-    #adding a descritive text of the first sim
+    #adding a descriptive text of the first sim
     print(" Now will you run or Walk away from a stranger in black? \n\n ")
-    print("A wierd looking men in black with a hat on sitting at the back of a strange house calling your full name\n \n ")
+    print("A weird looking men in black with a hat on sitting at the back of a strange house calling your full name\n \n ")
     print("1 - Run")
     print("2- Walk away")
 
@@ -56,10 +56,10 @@ while True:
     #getting user input or answer and converting it into a variable
     action1 = input("Choose your action (1 or 2 )\n \n:")
     
-    #start our if statement to macth or loop user input
+    #start our if statement to match or loop user input
     if action1 == "1":
         
-        #determing the out come of user action taken
+        #determing the outcome of user action taken
         print(" So you run and got to a point where there is no way but 3 tall walls and the way you coming from\n")
         print(" You looked back to see this stranger coming towards you")
         print(" 1 - Scream for help")
@@ -71,7 +71,7 @@ while True:
         
         if action2 == "1":
             
-            #determing the out come of user action taken
+            #determing the outcome of user action taken
             print(" So you Scream for help\n")
             print(" You looked back to see who was coming to help but another men in black stranger coming towards you")
             print(" 1 - Get ready to fight")
@@ -83,7 +83,7 @@ while True:
             
             if action3 == "1":
                     
-                    #determing the out come of user action taken
+                    #determing the outcome of user action taken
                     print(" So you are ready to fight\n")
                     print(" The strangers looked at each other and laugh")
                     print(" 1 - Attack first")
@@ -94,21 +94,21 @@ while True:
                 
                 
                     if action4 == "1":
-                        #determing the out come of user action taken
+                        #determing the outcome of user action taken
                         print(" So you decided to resolve with violence,\n sorry but you lose, \n that was your parents playing prank on you.\n")
                     
                     
-                    # else if is used to make other options possible for user    
+                    # else if is used to make other options possible for the user    
                     elif action4 == "2":
                         #    
                         print(" The strangers looked at each other and laugh because you won, \n its just your parents in prank")
                         
-                        #else to ask user to stay according to code
+                        #else to ask the user to stay according to the code
                     else:
                         print("Please choose from only 1 or 2 for you action, thank you")   
 
 
-    # else if is used to make other options possible for user
+    # else if is used to make other options possible for the user
     elif action1 == "2":
         
         print(" So you decided to walk away but the stranger follows you") 
@@ -116,7 +116,7 @@ while True:
         print(" The stranger just looked at you and laugh")
         
         
-        #determing the out come of user action taken
+        #determing the outcome of user action taken
         print(" 1 - Attack the stranger first")
         print(" 2 - Ask the stranger what is the sense in all this?")
                         
@@ -134,18 +134,18 @@ while True:
         print(" The strangers looked at each other and laugh because you won, \n its just your parents in prank")  
     
     
-    #else to ask user to stay according to code
+    #else to ask the user to stay according to the code
     else:
                         
         print("Please choose from only 1 or 2 for you action, thank you")
     
     
                         
-    # asking user wherther they want to continue the game                    
+    # asking users whether they want to continue the game                    
     replay = input("Do you want to try again? (Yes/No) :  ")
     
     
-    #if user type yes the game starts over
+    #if the user types yes the game starts over
     if replay.upper() != "YES" :
         print(" Thank You - Game Over!!!")
         break
